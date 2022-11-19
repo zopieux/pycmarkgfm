2c31829e37b9d9be26d442e4a3cc0bbcdc9670a8
Alexandre Macabies <web+oss@zopieux.com>
2022-11-19 17:15:23 +0100

    Add CHANGELOG, bump to v1.2.0.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+### 1.2.0
+
+* No new feature nor bugfix.
+* Bump cmark version to `0.29.0.gfm.6`. This changes the syntax of footnote IDs.
+* Drop tests and binary releases for Python 3.5 and 3.6.
+
+### 1.1.0
+
+* Beginning of this changelog.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name='pycmarkgfm',
-    version='1.1.0',
+    version='1.2.0',
     description="Bindings to GitHub's Flavored Markdown (cmark-gfm), with enhanced support for task lists.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,11 +20,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     packages=find_packages(),
     cffi_modules=["pycmarkgfm/build_cmark.py:ffibuilder"],
