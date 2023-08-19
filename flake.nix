9a79238b8da22b60fae3548c5703ee241a2b4549
Alexandre Macabies <web+oss@zopieux.com>
2023-08-19 19:40:14 +0200

    Add flake with devShell.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in with pkgs;
+    {
+      devShells.default = mkShell {
+        buildInputs = [ cmake yamlfix python311 ];
+      };
+    }
+  );
+}
